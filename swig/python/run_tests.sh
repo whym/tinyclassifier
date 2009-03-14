b3a584d3ab8ca2c09f8218de941bdc2f071c52c7
whym <whym@livedoor.com>
2009-03-14 11:35:42 +0900

    * fix for LD_LIBRARY_PATH etc

--- swig/python/run_tests.sh
+++ swig/python/run_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-export LD_LIBRARY_PATH="../../lib"
-export PYTHONPATH="build/lib.linux-i686-2.5"
+export LD_LIBRARY_PATH="../../lib:${LD_LIBRARY_PATH}"
+export PYTHONPATH=build/lib.linux-`uname -m`-`python --version 2>&1 |  sed 's/^[^ ]* \(2\.[0-9]*\).*/\1/'`
 python test_perceptron.py
